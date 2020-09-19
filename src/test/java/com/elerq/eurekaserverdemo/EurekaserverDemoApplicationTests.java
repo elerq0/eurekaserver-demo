ca265b6b4cc3f9b7f6c89eb0a83c8b56f40d5fc4
elerq <elerq0@gmail.com>
2020-09-19 22:38:22 +0200

    init

---
+++ src/test/java/com/elerq/eurekaserverdemo/EurekaserverDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elerq.eurekaserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
